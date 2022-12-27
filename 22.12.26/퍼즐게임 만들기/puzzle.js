f3acfd85d32fc806c1920737f0fd72e13e603455
DESKTOP-SU1D8SB\wlsdn <wlsdnr3636@gmail.com>
2022-12-27 20:19:31 +0900

    22.12.27. 자동 클리어 버튼 추가,클리어or패배 시 그림 안누림 추가/근데 왜 드래그 느려짐?진짜모름

--- 22.12.26/퍼즐게임 만들기/puzzle.js
+++ 22.12.26/퍼즐게임 만들기/puzzle.js
@@ -1,10 +1,13 @@
 const container = document.querySelector('.image-container')
 const startButton = document.querySelector('.start-button')
 const gameText = document.querySelector('.game-text')
+const loseText = document.querySelector('.lose-text')
 const playTime = document.querySelector('.play-time')
 const tiles = document.querySelectorAll(".image-container > li")
-const hintButton = document.querySelector(".clear-button")
+const hintButton = document.querySelector(".hint-button")
+const clearButton = document.querySelector(".clear-button")
 
+let hintCheck = false;
 let isPlaying = false;
 let timeInterval = null;
 let time = 0;
@@ -15,31 +18,79 @@ const draged = {
     index : null
 }
 
-hintButton.addEventListener("click",()=>{
-    [...container.children].forEach(child=>{
-        child.innerText = child.getAttribute("data-type")
-    })
+clearButton.addEventListener("click",()=>{
+    console.log(container.children)
+    const checkBox = [...container.children]
+    console.log(checkBox)
+    console.log()
+        let unMatched =null;
+        checkBox.forEach(child=>{
+            draged.el = child;
+            draged.class = child.className
+            draged.index = checkBox.indexOf(child)
+                for(let i=0; i<checkBox.length; i++){
+                    
+                    if(child.dataset.type == i){
+                        console.log('hello')
+                        checkBox[i].after(child);
+                        checkBox[draged.index].after(checkBox[i]);
+                    }
+                    
+                }
+                child.innerText = child.getAttribute("data-type")
+        })
+        unMatched = checkBox.filter((list,index)=>{
+            return list.getAttribute("data-type") != index
+        })
+        if(unMatched.length === 0){
+            isPlaying = false;
+            clearInterval(timeInterval)
+            gameText.style.display = "block"
+        }
+        console.log(unMatched.length)
 })
+hintButton.addEventListener("click",()=>{
+
+    hintCheck == true ? hintCheck = false : hintCheck = true;
+    if(hintCheck == true){
+        [...container.children].forEach(child=>{
+            child.innerText = child.getAttribute("data-type")
+        })
+    }else{
+        [...container.children].forEach(child=>{
+            child.innerText = ""
+        })
+    }
+})
+
 startButton.addEventListener("click",()=>{
     setGame();
 })
 function setGame(){
-    time = 0;
+    isPlaying = true;
+    time = 5;
+    playTime.innerText = time
+    loseText.style.display = "none"
     gameText.style.display = "none"
     timeInterval = setInterval(()=>{
-        time++;
-        playTime.innerText = time
+        if(time==0){
+            clearInterval(timeInterval)
+            loseGame()
+            isPlaying = false;
+        }else{
+            time--;
+            playTime.innerText = time
+        }
     },1000)
     const gameTiles = shuffle([...tiles])
     container.innerHTML = ""
     gameTiles.forEach(tile=>{
         container.appendChild(tile)
     })
-    console.log(gameTiles)
 }
-
-const testArray = [0,1,2,3,4,5,6,7,8,9]
-shuffle(testArray)
+function loseGame(){
+    loseText.style.display = "block"
+}
 function shuffle(array){
     let index = array.length -1;
     while(index >0){
@@ -59,37 +110,46 @@ function checkStatus(){
         isPlaying = false;
         clearInterval(timeInterval)
         gameText.style.display = "block"
+        isPlaying = false;
     }
+    tiles.forEach(tile =>{
+        tile.draggable = false
+    })
     /* currentList.forEach((list,index)=>{
         console.log(list,index)
     }) */
     console.log(unMatched)
 }
-
 container.addEventListener('dragstart',e =>{
-    const obj = e.target;
-    draged.el = obj;
-    draged.class = obj.className;
-    draged.index = [...obj.parentNode.children].indexOf(obj);
-    //console.log(e)
+    if(isPlaying ==true){
+        const obj = e.target;
+        draged.el = obj;
+        draged.class = obj.className;
+        draged.index = [...obj.parentNode.children].indexOf(obj);
+        //console.log(e)
+    }
 })
 container.addEventListener('dragover', e =>{
-    e.preventDefault()
-    //console.log(e)
+    if(isPlaying ==true){
+        e.preventDefault()
+        //console.log(e)
+    }
 })
 container.addEventListener('drop', e =>{
-    console.log(draged.el.nextSibling)
-    const obj = e.target;
-    let originPlace;
-    let isLast = false;
-    if(draged.el.nextSibling){
-        originPlace = draged.el.nextSibling;
-    }else{
-        originPlace = draged.el.previousSibling;
-        isLast = true
+    if(isPlaying ==true){
+        console.log(draged.el.nextSibling)
+        const obj = e.target;
+        let originPlace;
+        let isLast = false;
+        if(draged.el.nextSibling){
+            originPlace = draged.el.nextSibling;
+        }else{
+            originPlace = draged.el.previousSibling;
+            isLast = true
+        }
+        const droppedIndex = [...obj.parentNode.children].indexOf(obj);
+        draged.index > droppedIndex ? obj.before(draged.el) : obj.after(draged.el) //?가 의미가 있나?
+        isLast ? originPlace.after(obj) : originPlace.before(obj)
+        checkStatus()
     }
-    const droppedIndex = [...obj.parentNode.children].indexOf(obj);
-    draged.index > droppedIndex ? obj.before(draged.el) : obj.after(draged.el) //?가 의미가 있나?
-    isLast ? originPlace.after(obj) : originPlace.before(obj)
-    checkStatus()
 })
